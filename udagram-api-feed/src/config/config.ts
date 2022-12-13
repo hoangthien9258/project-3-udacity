704af269445d3de851b1f43a1530bb42c7eb57fd
hoangthien9258 <49122801+hoangthien9258@users.noreply.github.com>
2022-12-13 18:32:06 +0700

    Add files via upload

---
+++ udagram-api-feed/src/config/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+  'username': 'postgres',
+  'password': 'postgres',
+  'database': 'postgres',
+  'host': 'postgres.ce44jbfv81hh.us-east-1.rds.amazonaws.com',
+  'dialect': 'postgres',
+  'aws_region': 'us-east-1',
+  'aws_profile': 'default',
+  'aws_media_bucket': 'project-3-bucket-udacity',
+  'url': 'http://localhost:8100',
+  'jwt': {
+    'secret': 'testing',
+  },
+};
